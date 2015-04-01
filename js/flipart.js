1726b44da48a05d8d744d24aa96d470da26a6d18
Vuk Thinkpad Ubuntu <djapic@yandex.ru>
2015-04-01 19:52:29 +0200

    Flipart. First full working version

--- js/flipart.js
+++ js/flipart.js
@@ -29,10 +29,19 @@ function resizeGameWindow(phase) {
 
 }
 
+/**callback function when single game is started */
 function onStartSingleDataRetrieved() {
     resizeGameWindow('startsingle');
     displayGameWindow('dGame');
     flipart.mouseMoves.init('dActions');
+}
+
+/**on flip action */
+function flip(isHorizontal) {
+    var rec = flipart.getRectangle( flipart.mouseMoves.start, flipart.mouseMoves.end );
+    flipart.flipMatrix(rec, isHorizontal);
+    flipart.updateBackground(rec);
+    return false;
 }
 
 var flipart = {
@@ -48,6 +57,8 @@ var flipart = {
         , 'instructions': 'inst'
     }
     , gameOptions: null     //gameOptions object retrieved from server
+    , matrix: null          //transformation matrix
+    , transformations: []
 
             /** computes game window dimensions */
     , computeSize: function (winWidth, winHeight) {
@@ -82,6 +93,7 @@ var flipart = {
             dataType: 'json'
         }).done(function (data) {
             fa.gameOptions = data.gameOptions;
+            fa.matrix = data.matrix;
             var mes = data.gameOptions.measures;
             pf.width(mes.w).height(mes.h);
             for (i = 0; i < mes.n; i++) {
@@ -99,13 +111,77 @@ var flipart = {
                     }));
                 }
             }
-            $('.tile', tiles).css('background-image', 'url(' + fa.urls.image + ')');
+            //$(document.body).append('<img src="'+fa.urls.image+'" />')
+            $('.tile', tiles).css('background-image', 'url(' + fa.urls.image + '?t='+new Date().getTime()+')');
             if (callback) {
                 callback();
             }
         });
     }
-
+    /** for two diagonal ends of rectangle returns upper left and lower right point */
+    , getRectangle: function (start, end) {
+        var imin, imax, jmin, jmax;
+        if (start.i < end.i) {
+            imin = start.i;
+            imax = end.i;
+        } else {
+            imin = end.i;
+            imax = start.i;
+        }
+        if (start.j < end.j) {
+            jmin = start.j;
+            jmax = end.j;
+        } else {
+            jmin = end.j;
+            jmax = start.j;
+        }
+        return {imin: imin, imax: imax, jmin: jmin, jmax: jmax};
+    }
+    /**flips part of flipart.matrix
+     * 
+     * @param {type} rec rectangle
+     */
+    , flipMatrix: function (rec,isHorizontal) {
+        var mat =new Array(rec.imax - rec.imin +1);
+        var i,j,f;
+        for(i=0; i<=rec.imax-rec.imin; i++){
+            mat[i] =new Array(rec.jmax - rec.jmin +1);
+            for(j=0; j<=rec.jmax-rec.jmin; j++){
+                if(isHorizontal){
+                    f =flipart.matrix[rec.imin + i][rec.jmax-j];
+                    mat[i][j] ={ i:f.i, j:f.j, hflip:!f.hflip, vflip:f.vflip };
+                } else {
+                    f =flipart.matrix[rec.imax - i][rec.jmin+j];
+                    mat[i][j] ={ i:f.i, j:f.j, hflip:f.hflip, vflip:!f.vflip };
+                }
+            }
+        }
+        for(i=0; i<=rec.imax-rec.imin; i++){
+            for(j=0; j<=rec.jmax-rec.jmin; j++){
+                this.matrix[rec.imin+i][rec.jmin+j] =mat[i][j];
+            }
+        }
+    }
+    , updateBackground: function(rec){
+        var imin =rec.imin;
+        var imax =rec.imax;
+        var jmin =rec.jmin;
+        var jmax =rec.jmax;
+        var i,j, f, pozx, pozy;
+        var mes =this.gameOptions.measures;
+        var tem =_.template( $('#ttile').data('tileid') );
+        var id;
+        for(i=imin;i<=imax;i++){
+            for(j=jmin;j<=jmax;j++){
+               f =this.matrix[i][j];
+               pozx =f.hflip? (2*mes.w -(f.j+1)*mes.tileWidth): f.j*mes.tileWidth; 
+               pozy =f.vflip? (2*mes.h -(f.i+1)*mes.tileHeight): f.i*mes.tileHeight;  
+               id =tem({i:i,j:j});
+               $('#'+id).css('background-position', (-pozx)+'px '+ (-pozy) + 'px');
+               //$('#'+id).css('background-position', '0px 0px');
+            }
+        }
+    }
     , mouseMoves: {
         isdown: false
         , ismove: false
@@ -114,103 +190,114 @@ var flipart = {
         , offleft: null
         , start: {i: null, j: null}    //start position of selection
         , end: {i: null, j: null}
-        , blockTemplate :null           //underscore template for block id
-        , border :null                  //border for whole selected area
+        , blockTemplate: null           //underscore template for block id (function)
+        , border: null                  //border for whole selected area
         , tileWidth: null               //from gameOptions.measures
         , tileHeight: null
         , init: function (actionsDiv) {
-            var act =$('#'+actionsDiv);
+            var act = $('#' + actionsDiv);
             var ofs = act.offset();
             act.mousedown(function (event) {
                 flipart.mouseMoves.down(event);
             })
-            .mousemove(function (event) {
-                flipart.mouseMoves.move(event);
-            })
-            .mouseup(function (event) {
-                flipart.mouseMoves.up(event);
-            });
+                    .mousemove(function (event) {
+                        flipart.mouseMoves.move(event);
+                    })
+                    .mouseup(function (event) {
+                        flipart.mouseMoves.up(event);
+                    });
             this.offtop = ofs.top;
             this.offleft = ofs.left;
-            this.blockTemplate =_.template($('#tblock').data('blockid'));
-            this.border =$('#tblock').data('border');
-            this.tileWidth =flipart.gameOptions.measures.tileWidth;
-            this.tileHeight =flipart.gameOptions.measures.tileHeight;
+            this.blockTemplate = _.template($('#tblock').data('blockid'));
+            this.border = $('#tblock').data('border');
+            this.tileWidth = flipart.gameOptions.measures.tileWidth;
+            this.tileHeight = flipart.gameOptions.measures.tileHeight;
         }
         , down: function (ev) {
             this.isdown = true;
-            var blk =this._getCoordsForMouse(ev);
-            var blockid = _;
+            var blk = this._getCoordsForMouse(ev);
+            var blockid;
+            if (this.state == 'empty' || this.state == 'click2') {
+                $('div.block').removeClass('selected').css('border', 'none');
+                $('#dHorflip, #dVerflip').css('display', 'none');
+            }
             switch (this.state) {
                 case 'empty':
                     this.start.i = blk.i;
                     this.start.j = blk.j;
-                    blockid =this.blockTemplate({i:blk.i,j:blk.j});
-                    $('#'+blockid).addClass('selected');        
-                    this.state ='click1';
+                    blockid = this.blockTemplate({i: blk.i, j: blk.j});
+                    $('#' + blockid).addClass('selected');
+                    this.state = 'click1';
+                    break;
+                case 'click2':
+                    this.state = 'empty';
+                    if(ev.target.id=='horflip'){
+                        flip(true);
+                    } else if(ev.target.id=='verflip'){
+                        flip(false);
+                    }
                     break;
             }
         }
         , move: function (ev) {
-            var i,j;
-            var ids =[];
+            var i, j;
+            var ids = [];
             var jid;
-            if(this.state!='click1'){
+            if (this.state != 'click1') {
                 return;
             }
             this.ismove = true;
-            var blk =this._getCoordsForMouse(ev);
-            var imin, imax, jmin, jmax;
-            if(this.start.i < blk.i ){
-                imin =this.start.i;
-                imax =blk.i;
-            } else {
-                imin =blk.i;
-                imax =this.start.i;
-            }
-            if(this.start.j < blk.j ){
-                jmin =this.start.j;
-                jmax =blk.j;
-            } else {
-                jmin =blk.j;
-                jmax =this.start.j;
-            }
-            $('div.block').removeClass('selected').css('border','none');
-            for(i=imin;i<=imax;i++){
-                for(j=jmin;j<=jmax;j++){
-                    jid ='#' + this.blockTemplate({i:i, j:j});
-                    ids.push( jid );
-                    if(i==imin){
-                        $(jid).css('border-top',this.border);
+            var blk = this._getCoordsForMouse(ev);
+            this.end.i = blk.i;
+            this.end.j = blk.j;
+            var rec = flipart.getRectangle(this.start, this.end);
+            $('div.block').removeClass('selected').css('border', 'none');
+            for (i = rec.imin; i <= rec.imax; i++) {
+                for (j = rec.jmin; j <= rec.jmax; j++) {
+                    jid = '#' + this.blockTemplate({i: i, j: j});
+                    ids.push(jid);
+                    if (i == rec.imin) {
+                        $(jid).css('border-top', this.border);
                     }
-                    if(j==jmin){
-                        $(jid).css('border-left',this.border);
+                    if (j == rec.jmin) {
+                        $(jid).css('border-left', this.border);
                     }
-                    if(i==imax){
-                        $(jid).css('border-bottom',this.border);
+                    if (i == rec.imax) {
+                        $(jid).css('border-bottom', this.border);
                     }
-                    if(j==jmax){
-                        $(jid).css('border-right',this.border);
+                    if (j == rec.jmax) {
+                        $(jid).css('border-right', this.border);
                     }
                 }
             }
             $(ids.toString()).addClass('selected');
-            //console.log('move at: ' + ids);
         }
         , up: function (ev) {
-            if(this.state=='click1' && this.ismove){
-                this.state ='click2';
+            if (this.state == 'click1' && this.ismove) {
+                this._rectangleSelected();
+                this.state = 'click2';
             }
             this.isdown = false;
             this.ismove = false;
         }
-        , _getCoordsForMouse: function(event){
+        /** for mouse click in picture get tile coordinates */
+        , _getCoordsForMouse: function (event) {
             var x = event.pageX - this.offleft;
             var y = event.pageY - this.offtop;
             var i = Math.floor(y / this.tileHeight);
             var j = Math.floor(x / this.tileWidth);
             //console.log('down at: ' + x + ', ' + y + ' >>ij: ' + i + ',' + j);
-            return {i:i, j:j};
+            return {i: i, j: j};
+        }
+        , _rectangleSelected: function () {
+            var rec = flipart.getRectangle(this.start, this.end);
+            var x1, x2, y1, y2;
+            x1 = rec.jmin * this.tileWidth;
+            x2 = (rec.jmax + 1) * this.tileWidth;
+            y1 = rec.imin * this.tileHeight;
+            y2 = (rec.imax + 1) * this.tileHeight;
+            $('#dHorflip').css({display: 'block', top: y1 + 'px', left: x1 + 'px', width: (x2 - x1) + 'px'});
+            $('#dVerflip').css({display: 'block', top: (y1 - 10 + (y2 - y1) / 2) + 'px', left: x1 + 'px'});
         }
     }
 
